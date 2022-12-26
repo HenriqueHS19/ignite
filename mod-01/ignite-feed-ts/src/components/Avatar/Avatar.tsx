6f44f468278d993a333b37bd4d00bcfe628d80d7
HenriqueHS19 <henriquesantos.hs34@gmail.com>
2022-12-26 20:44:51 -0300

    udpate interface file Avatar.tsx

--- mod-01/ignite-feed-ts/src/components/Avatar/Avatar.tsx
+++ mod-01/ignite-feed-ts/src/components/Avatar/Avatar.tsx
@@ -1,12 +1,16 @@
+import { ImgHTMLAttributes } from 'react';
 import styles from './styles.module.css';
 
-interface IAvatar {
+interface IAvatar extends ImgHTMLAttributes<HTMLImageElement>{
     src: string;
     hasBorder?: boolean;
 }
 
-export function Avatar({ src, hasBorder = true }: IAvatar) {
+export function Avatar({ hasBorder = true, ...rest }: IAvatar) {
     return (
-        <img className={ hasBorder ? styles.avatar : styles.hasBorder } src={ src } />
+        <img
+            className={ hasBorder ? styles.avatar : styles.hasBorder }
+            {...rest}
+        />
     );
 }
